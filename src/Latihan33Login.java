087c861189ab34362f45e105f252ddc62314cc59
aldilesmana <aldilesmana181@gmail.com>
2018-10-09 00:30:54 +0700

    first commit

---
+++ src/Latihan33Login.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+//NAMA  :Aldi Lesmana
+//NIM   :10117108    
+//KELAS :IF-3
+
+/**
+ *
+ * @author RIA FITRIANI
+ */
+public class Latihan33Login {
+     private static String usName;
+    private static String passWord;
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Masukkan Username = ");
+        usName = scn.next();
+
+        System.out.print("Masukkan Password = ");
+        passWord = scn.next();
+
+        User cek = new User();
+        cek.pengecekkanLogin(usName,passWord);
+
+    }
+}
+
